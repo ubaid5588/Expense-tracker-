9a1d40a9da70c8dca3f82767f60ebc7ed6cdb51e
Muhammad Ubaid <Muhammadubaid5588@gmail.com>
2026-07-17 17:28:41 +0500

    Creating a project for an expense tracker.

---
+++ Expense Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expense Tracker
+//
+//  Created by Muhammad Ubaid on 17/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Expensive Tracker")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Muhammad Ubaid on 17/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
